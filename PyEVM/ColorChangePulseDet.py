8c1caa5e050da6f1f446657e33bc430a141c6315
Anirudh0707 <yani.bh@gmail.com>
2020-05-21 02:46:39 +0530

    Cleaning the Color based Pulse Detection Code

--- PyEVM/ColorChangePulseDet.py
+++ PyEVM/ColorChangePulseDet.py
@@ -14,9 +14,6 @@ ROOT = './Dataset/Data'
 JSON_FOLDER = './Dataset/JSON'
 HAAR = './resources/haarcascade_frontalface_default.xml'
 GT = (68.11, 71.82, 53.44, 61.18, 46.51, 65.39, 126.89)
-LEVELS = 3
-AMPLIFICATION = 20
-
 
 
 def harrCascadeFaceDet(image):
@@ -26,10 +23,10 @@ def harrCascadeFaceDet(image):
     x = int(x + 0.25 * w)
     w = int(w * 0.5)
     h = int(h * 0.9)
-    w = w + int(2**LEVELS)
-    w = w - (w % int(2**LEVELS))
-    h = h + int(2**LEVELS)
-    h = h - (h % int(2**LEVELS))
+    w = w + int(2**levels)
+    w = w - (w % int(2**levels))
+    h = h + int(2**levels)
+    h = h - (h % int(2**levels))
     return (x,y,w,h)
 
 def getFace(image, faceTuple):
@@ -70,9 +67,12 @@ if __name__ == '__main__':
     video_filenames = os.listdir(ROOT)
     face_cascade = cv2.CascadeClassifier(HAAR)
     inputNumer = int(input("Enter a Num from 0 to 6 :: "))
-    cap = cv2.VideoCapture(os.path.join(ROOT,video_filenames[inputNumer]))
+    cap = cv2.VideoCapture(os.path.join(ROOT,video_filenames[inputNumer])) # 
     assert cap.isOpened(), 'Cannot capture source'
 
+    # Paramters for this Code
+    levels = 3
+    amplification = 20
     counter = 0
     inFr  = 30
     outFr = 60
@@ -90,32 +90,45 @@ if __name__ == '__main__':
         counter += 1
     cap.release()
     videoTensor = np.array(videoTensor)
-    print(videoTensor.shape)
     print("Video Tensor Ready")
-    gau_video = EVM.gaussian_video(videoTensor,levels=LEVELS)
+    gau_video = EVM.gaussian_video(videoTensor,levels=levels)
     print("Pyramid Fomation Complete")
     filtered_tensor = EVM.temporal_ideal_filter(gau_video,0.8,3,inFr)
     print("Filter Complete")
-    amplifiedVideo = filtered_tensor * AMPLIFICATION
-    reconstructedTensor = EVM.reconstructVideo(amplifiedVideo,videoTensor,levels=LEVELS)
+    amplifiedVideo = filtered_tensor * amplification
+    reconstructedTensor = EVM.reconstructVideo(amplifiedVideo,videoTensor,levels=levels)
     print("Reconstruction Complete")
-    # EVM.save_video(reconstructedTensor)
-    print(reconstructedTensor.shape)
 
     # Sum over Width and Breadth to get the Time vs RGB components
-    # Note 1: Format is BGR due to OpenCV
+    # Note 1: Format is B,G,R due to OpenCV
     # Note 2: Using 2 lines for computing mean as only the recent numpy library has multi axis summing in a sinlge command(acc to th numpy doc website)
     BGRComponents = np.mean(reconstructedTensor, axis = 1)
     BGRComponents = np.mean(BGRComponents, axis = 1)
-    # BGRComponents = np.mean(reconstructedTensor, axis = (1,2)) # Use when numpy>=1.7.0
-    print(BGRComponents.shape)
-    # PCA 
-    pca = PCA(n_components = 3)
-    principalComponents = pca.fit_transform(BGRComponents)
-    # for i in range(3):
-    #     fftData = np.fft.fft()
-    # Or Just take Green Channel
-    outputSignal = BGRComponents[:,1]
-    plt.plot(outputSignal)
+    BGRComponents = interpolationAndFiltering(BGRComponents, inFr, outFr)
+    # Extract Components
+    model = PCA(n_components = 3)
+    principalComponents = model.fit_transform(BGRComponents)
+    nyquist = int(len(principalComponents)/2)  
+    x_disp = outFr/2*np.arange(nyquist)/nyquist
+    # Selecte Best Component. Usually the Second one i.e Index = 1
+    powerRatio = []
+    listForDistanceEstimation = []
+    for i in range(3):
+        fftData = np.fft.fft(principalComponents[:,i])[0:nyquist]
+        powerSpectrum = np.abs(fftData)**2
+        maxFreq = np.argmax(powerSpectrum)
+        powerInMaxFreq = np.sum(powerSpectrum[maxFreq-1:maxFreq+2]) + np.sum(powerSpectrum[2*maxFreq:2*maxFreq+3])
+        powerRatio.append(powerInMaxFreq/np.sum(powerSpectrum))
+        listForDistanceEstimation.append((maxFreq)/nyquist*outFr/2)
+    # Choose Signal 
+    PCAIndex = np.argmax(np.array(powerRatio))
+    chosenSignal = principalComponents[:,PCAIndex]
+    # Peak Det and Display
+    distance = int(outFr*2/listForDistanceEstimation[PCAIndex])-10 # Emperically found realtion
+    print(distance)
+    print("Target ", GT[inputNumer])
+    peaks, _ = signal.find_peaks(chosenSignal, distance=distance)
+    plt.plot(chosenSignal)
+    plt.plot(peaks, chosenSignal[peaks], "x")
+    plt.title("Avg Heart Beat = "+str(60*60/(peaks[-1] - peaks[0])*len(peaks)))
     plt.show()
-    
